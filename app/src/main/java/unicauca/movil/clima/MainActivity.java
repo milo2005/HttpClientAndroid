73741b7f8330b0a2e8a39ed5a94658c00a5fbb46
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-12-04 17:08:48 -0500

    Proyect Basic

---
+++ app/src/main/java/unicauca/movil/clima/MainActivity.java
@@ -0,0 +1,25 @@
+package unicauca.movil.clima;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import unicauca.movil.clima.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+
+    private void setWeather(String text, String temp, String pres, String hum){
+        binding.setHum(hum);
+        binding.setPres(pres);
+        binding.setTemp(temp);
+        binding.setText(text);
+    }
+}
